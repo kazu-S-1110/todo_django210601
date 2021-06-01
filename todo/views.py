ae333f8464d83290dd1d784f76f47d3f7552be9e
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-02 08:22:53 +0900

    DetailViewを実装、urlの指定方法を理解

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from .models import TodoModel
 
 
 class TodoList(ListView):
     template_name = "list.html"  # htmlファイルを指定する
     model = TodoModel  # modelを指定する（このクラスが呼び出すデータベースを指定している感じ）
+
+
+class TodoDetail(DetailView):
+    template_name = "detail.html"
+    model = TodoModel
