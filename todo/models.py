a9c75b879465600697fb30c372b1c9f401001460
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-02 08:11:20 +0900

    listViewを理解、htmlでobject表示

--- todo/models.py
+++ todo/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class TodoModel(models.Model):  # model.Modelを継承して作成している
     title = models.CharField(max_length=30)
     memo = models.TextField()
+
+    def __str__(self):
+        return self.title
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import TodoModel
 
-# Create your views here.
+
+class TodoList(ListView):
+    template_name = "list.html"  # htmlファイルを指定する
+    model = TodoModel  # modelを指定する（このクラスが呼び出すデータベースを指定している感じ）
