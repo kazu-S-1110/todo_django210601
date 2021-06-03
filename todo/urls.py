7bcb430798faec9f0f64c030e2db30869842ad94
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-04 08:23:14 +0900

    createViewを実装。form.as_*結構便利

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import TodoList, TodoDetail
+from .views import TodoList, TodoDetail, TodoCreate
 
 urlpatterns = [
     path("list", TodoList.as_view()),
-    path("detail/<int:pk>", TodoDetail.as_view()) # pk(primary keyが必要となるため指定してあげる)
+    path("detail/<int:pk>", TodoDetail.as_view()),  # pk(primary keyが必要となるため指定してあげる)
+    path("create/", TodoCreate.as_view()),
 ]
