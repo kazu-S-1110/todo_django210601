ab6ac85b0afe6f92951cc6f4ff5a3d4cfb04fae3
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-04 12:30:35 +0900

    DeleteViewを実装

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import TodoList, TodoDetail, TodoCreate
+from .views import TodoList, TodoDetail, TodoCreate, TodoDelete
 
 urlpatterns = [
     path("list", TodoList.as_view(), name="list"),
     path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),  # pk(primary keyが必要となるため指定してあげる)
     path("create/", TodoCreate.as_view(), name="create"),  # nameを付与してreversed_lazyで指定可能にする
+    path("delete/<int:pk>", TodoDelete.as_view(), name="delete"),
 ]
