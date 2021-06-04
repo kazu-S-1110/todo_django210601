6e2aaf8497c01b3759ae92dd71da1b89b1e9273f
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-04 12:40:07 +0900

    updateViewを実装。

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import TodoList, TodoDetail, TodoCreate, TodoDelete
+from .views import TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate
 
 urlpatterns = [
     path("list", TodoList.as_view(), name="list"),
     path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),  # pk(primary keyが必要となるため指定してあげる)
     path("create/", TodoCreate.as_view(), name="create"),  # nameを付与してreversed_lazyで指定可能にする
     path("delete/<int:pk>", TodoDelete.as_view(), name="delete"),
+    path("update/<int:pk>", TodoUpdate.as_view(), name="update")
 ]
