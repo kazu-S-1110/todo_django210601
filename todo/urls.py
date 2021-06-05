bf6ba6450c2cf17cda3e84de51fe956fed52a7f7
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-06-05 20:11:23 +0900

    ページ遷移機能を実装。

--- todo/urls.py
+++ todo/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("detail/<int:pk>", TodoDetail.as_view(), name="detail"),  # pk(primary keyが必要となるため指定してあげる)
     path("create/", TodoCreate.as_view(), name="create"),  # nameを付与してreversed_lazyで指定可能にする
     path("delete/<int:pk>", TodoDelete.as_view(), name="delete"),
-    path("update/<int:pk>", TodoUpdate.as_view(), name="update")
+    path("update/<int:pk>", TodoUpdate.as_view(), name="update"),
+
 ]
